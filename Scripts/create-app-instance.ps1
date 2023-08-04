cc9a482ce863efe641f9f42fde1d1eaeb8411239
marcusruas <marcusruas.andrade@gmail.com>
2023-08-04 12:01:15 -0300

    Refactored docker publish

--- Scripts/create-app-instance.ps1
+++ Scripts/create-app-instance.ps1
@@ -1,14 +1,15 @@
 # Defina o nome da imagem e o nome do container desejados
-$ImageName = "worker_template_image"
-$ContainerName = "container_worker_template"
+$IMAGE_NAME = "worker_template_image"
+$CONTAINER_NAME = "container_worker_template"
+$NETWORK_NAME = "worker_network"
 
 Write-Output "Building the Docker Image"
 
 # Constrói a imagem Docker usando o Dockerfile
-docker build -t $ImageName .
+docker build -t $IMAGE_NAME ../.
 
 Write-Output "Running the app"
 
-docker run --name "$ContainerName" -p 8080:80 $ImageName
+docker run --name "$CONTAINER_NAME" -p 8080:80 $IMAGE_NAME --network $NETWORK_NAME
 
 Write-Output "App is running at port 8080"
